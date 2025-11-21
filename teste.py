fe43b62c487d262f6a2c5d900a54eeb536cb2284
iagovic <iago904c@gmail.com>
2025-11-21 20:46:10 -0300

    alou

--- teste.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-
-
-genai.configure(api_key="AIzaSyDFr_NUmjyc1UT3mJBEAI7-LljPUf5viDI")
-
-model = genai.GenerativeModel("gemini-2.0-flash")
-resp = model.generate_content("diga oi")
-print(resp.text)
